1a0018d7d55713622c815ea6d4f3c1cfc8a1a09b
Tim <hello@timsmart.co>
2023-12-01 13:47:07 +1300

    move cjs to root directory

---
+++ .changeset/many-mayflies-develop.md
@@ -0,0 +1,5 @@
+---
+"@effect/build-utils": patch
+---
+
+move cjs to root directory
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,29 @@
 #!/usr/bin/env node
 
-import * as CliApp from "@effect/cli/CliApp"
 import * as Command from "@effect/cli/Command"
+import * as NodeContext from "@effect/platform-node/NodeContext"
 import { runMain } from "@effect/platform-node/Runtime"
-import { Effect, Match, pipe } from "effect"
+import { Effect } from "effect"
 import * as PackV1 from "./PackV1"
 import * as PackV2 from "./PackV2"
 import * as PrepareV1 from "./PrepareV1"
 import * as PrepareV2 from "./PrepareV2"
 
-const packV1 = Command.make("pack-v1")
-const packV2 = Command.make("pack-v2")
-const prepareV1 = Command.make("prepare-v1")
-const prepareV2 = Command.make("prepare-v2")
-
-const buildUtils = pipe(
-  Command.make("build-utils"),
-  Command.subcommands([packV1, packV2, prepareV1, prepareV2]),
+const run = Command.make("build-utils").pipe(
+  Command.withSubcommands([
+    Command.make("pack-v1", {}, () => PackV1.run),
+    Command.make("pack-v2", {}, () => PackV2.run),
+    Command.make("prepare-v1", {}, () => PrepareV1.run),
+    Command.make("prepare-v2", {}, () => PrepareV2.run),
+  ]),
+  Command.run({
+    name: "Effect Build Utils",
+    version: "0.0.0",
+  }),
 )
 
-const cli = CliApp.make({
-  name: "Effect Build Utils",
-  version: "0.0.0",
-  command: buildUtils,
-})
-
-const handleCommand = Match.type<
-  Command.Command.GetParsedType<typeof buildUtils>
->().pipe(
-  Match.when(
-    { subcommand: { _tag: "Some", value: { name: "pack-v1" } } },
-    () => PackV1.run,
-  ),
-  Match.when(
-    { subcommand: { _tag: "Some", value: { name: "pack-v2" } } },
-    () => PackV2.run,
-  ),
-  Match.when(
-    { subcommand: { _tag: "Some", value: { name: "prepare-v1" } } },
-    () => PrepareV1.run,
-  ),
-  Match.when(
-    { subcommand: { _tag: "Some", value: { name: "prepare-v2" } } },
-    () => PrepareV2.run,
-  ),
-  Match.orElse(() => Effect.dieMessage("unknown command")),
-)
-
-Effect.sync(() => process.argv.slice(2)).pipe(
-  Effect.flatMap(args => CliApp.run(cli, args, handleCommand)),
+run(process.argv.slice(2)).pipe(
+  Effect.provide(NodeContext.layer),
   Effect.tapErrorCause(Effect.logError),
   runMain,
 )
